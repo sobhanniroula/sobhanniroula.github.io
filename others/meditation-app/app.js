8db06971288b5189a7e18ada2c906d995a2a1f80
sobhanniroula <sobhan_niroula@yahoo.com>
2019-07-28 21:54:18 +0300

    final update for meditation app

--- others/meditation-app/app.js
+++ others/meditation-app/app.js
@@ -39,8 +39,6 @@ replay.addEventListener("click", function() {
 const restartSong = song =>{
     let currentTime = song.currentTime;
     song.currentTime = 0;
-    console.log("ciao")
-
 }
 
 timeSelect.forEach(option => {
